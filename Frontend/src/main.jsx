ad3258c0a2c6176af5bdb7f2757ec6615b7137e2
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2023-08-26 01:45:34 +0530

    addedFiles

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { ThemeProvider, createTheme } from '@mui/material'
+import {} from '@mui/x-data-grid/themeAugmentation';
+const theme = createTheme({
+    components: {
+        MuiDataGrid: {
+          styleOverrides: {
+            root: {
+              backgroundColor: 'red',
+            },
+          },
+        },
+      },
+    typography: {
+        fontFamily: [
+            'Bebas Neue',
+            'Poppins',
+            'sans-serif',
+        ].join(',')},
+    });
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
+
+)
